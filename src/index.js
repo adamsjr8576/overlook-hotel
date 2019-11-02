8e5d0e31b7ae90e2f30181a3e24f199dda4cc664
John Adams <adamsjr8576@gmail.com>
2019-11-02 17:36:32 -0600

    Implement fetch for three datasets using romise.all

--- src/index.js
+++ src/index.js
@@ -7,8 +7,39 @@ import './images/login-background.jpg'
 import './images/silva-logo.png'
 import './images/user-page.png'
 
+let usersData;
+let roomsData;
+let bookingsData;
+let users;
+let rooms;
+let bookings;
+
 $("#login-submit-button").on('click', loginHandler);
 $("#user-header-logout").on('click', logout);
+getDataOnUserPageLoad();
+
+function getDataOnUserPageLoad() {
+  if($(".user-page-header").length > 0) {
+    usersData = getData('users/users', 'users');
+    roomsData = getData('rooms/rooms', 'rooms');
+    bookingsData = getData('bookings/bookings', 'bookings');
+    Promise.all([usersData, roomsData, bookingsData]).then((promise) => {
+      usersData = promise[0];
+      roomsData = promise[1];
+      bookingsData = promise[2];
+    }).then(data => {
+
+    });
+  }
+}
+
+function getData(type, dataName) {
+  const root = 'https://fe-apps.herokuapp.com/api/v1/overlook/1904/';
+  const url = `${root}${type}`;
+  const promise = fetch(url)
+  .then(response => response.json())
+  return promise;
+}
 
 function loginHandler() {
   let customerCheck = makeCustomerNameCheck();
